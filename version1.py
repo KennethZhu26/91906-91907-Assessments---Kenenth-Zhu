31cc667c7b57372434e09c93d681bfba6163400a
KennethZhu26 <kennethzhu09xd@gmail.com>
2026-08-09 17:43:12 +1200

    Added Sign Up Function

--- version1.py
+++ version1.py
@@ -15,3 +15,20 @@ def load_data(filename):
 def save_data(filename, data):
     with open(filename, "w") as file:
         json.dump(data, file, indent=4)
+
+def signup():
+    users = load_data("users.json")
+
+    username = input("Create username: ")
+    password = input("Create password: ")
+
+    new_student = Student(username, password)
+
+    users.append({
+        "username": new_student.username,
+        "password": new_student.password
+    })
+
+    save_data("users.json", users)
+
+    print("Account created successfully!")
