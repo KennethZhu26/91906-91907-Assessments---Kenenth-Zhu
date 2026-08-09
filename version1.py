e344b9bb93937bd94432f678d7a001fc5ab9d4fc
KennethZhu26 <kennethzhu09xd@gmail.com>
2026-08-09 18:10:52 +1200

    Sign Up Validation

--- version1.py
+++ version1.py
@@ -20,41 +20,6 @@ def signup():
     users = load_data("users.json")
 
     username = input("Create username: ")
-    password = input("Create password: ")
-
-    new_student = Student(username, password)
-
-    users.append({
-        "username": new_student.username,
-        "password": new_student.password
-    })
-
-    save_data("users.json", users)
-
-    print("Account created successfully!")
-
-    import json
-
-class Student:
-    def __init__(self, username, password):
-        self.username = username
-        self.password = password
-
-def load_data(filename):
-    try:
-        with open(filename, "r") as file:
-            return json.load(file)
-    except FileNotFoundError:
-        return []
-
-def save_data(filename, data):
-    with open(filename, "w") as file:
-        json.dump(data, file, indent=4)
-
-def signup():
-    users = load_data("users.json")
-
-    username = input("Create username: ")
 
     for user in users:
         if user["username"] == username:
