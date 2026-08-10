d8eddf3ea10534f8c69e9cf6b8317c77b7ea2953
KennethZhu26 <kennethzhu09xd@gmail.com>
2026-08-10 20:53:29 +1200

    Addition of Code Comments

--- version1.py
+++ version1.py
@@ -1,10 +1,19 @@
+#=============================
+# Opportunity Tracker Program
+#=============================
+
 import json
 
+# Classes
+
+# Stores the username and password of a student
+# Used when creating a new students' account
 class Student:
     def __init__(self, username, password):
         self.username = username
         self.password = password
 
+# Stores all information regarding avaliable opportunities
 class Opportunity:
     def __init__(self, id, name, type, organisation, deadline):
         self.id = id
@@ -13,6 +22,8 @@ class Opportunity:
         self.organisation = organisation
         self.deadline = deadline
 
+# Stores information regarding a student's application
+# This records who has applied, which opportunity they have applied for and application status
 class Application:
     def __init__(self, username, opportunity_id, opportunity_name, status):
         self.username = username
@@ -20,6 +31,10 @@ class Application:
         self.opportunity_name = opportunity_name
         self.status = status
 
+# File Handling
+
+# Loads data from JSON file and then returns as Python data
+# If file does not yet exist, empty list is returned instead of crashing
 def load_data(filename):
     try:
         with open(filename, "r") as file:
@@ -27,15 +42,20 @@ def load_data(filename):
     except FileNotFoundError:
         return []
 
+# Saves Python data to JSON file so that information can be used again even after restarting
 def save_data(filename, data):
     with open(filename, "w") as file:
         json.dump(data, file, indent=4)
 
+# Account Functions
+
+# Allows a new student to create an account and then saves their details to users.json
 def signup():
     users = load_data("users.json")
 
     username = input("Create username: ")
 
+    # Checks all existing users to make sure username is not already taken
     for user in users:
         if user["username"] == username:
             print("Username already exists.")
@@ -43,42 +63,54 @@ def signup():
 
     password = input("Create password: ")
 
+    # Checks that password created meets the minimum length requirement
     if len(password) < 6:
         print("Password must be at least 6 characters.")
         return None
 
+    # Creates a Student object using the new account details
     new_student = Student(username, password)
 
+    # Adds the new student's details to the list of users
     users.append({
         "username": new_student.username,
         "password": new_student.password
     })
 
+    # Saves the updated user list to the JSON file
     save_data("users.json", users)
 
     print("Account created successfully!")
     print("You are now logged in.")
 
+    # Returns the username so that student can be logged in immediately after sign up
     return username
 
+# Checks the username and password against the account details stored in users.json
 def login():
     users = load_data("users.json")
 
     username = input("Username: ")
     password = input("Password: ")
 
+    # Searches through each stored user to find matching login details
     for user in users:
         if user["username"] == username and user["password"] == password:
             print("Login successful!")
             return username
 
+    # Runs if no account matches login details entered
     print("Incorrect username or password.")
     return None
 
+# Opportunity Functions
+
+# Loads opportunities from the JSON file and the converts them into Opportunity objects
 def get_opportunities():
     data = load_data("opportunities.json")
     opportunities = []
 
+    # Goes through each opportunity stored in JSON file
     for item in data:
         opportunity = Opportunity(
             item["id"],
@@ -88,19 +120,23 @@ def get_opportunities():
             item["deadline"]
         )
 
+        # Adds each Opportunity object to opportunities list
         opportunities.append(opportunity)
 
     return opportunities
 
+# Displays all avaliable opportunities if selected
 def view_opportunities():
     opportunities = get_opportunities()
 
     print("\n===== Available Opportunities =====")
 
+    # Checks whether there are any opportunities to display
     if len(opportunities) == 0:
         print("No opportunities available.")
         return
 
+    # Displays the details of each avaliable opportunity
     for opportunity in opportunities:
         print("-------------------------")
         print("ID:", opportunity.id)
@@ -109,19 +145,23 @@ def view_opportunities():
         print("Organisation:", opportunity.organisation)
         print("Deadline:", opportunity.deadline)
 
+# Allows student to apply for opportunity using its corresponding ID
 def apply_opportunity(username):
     opportunities = get_opportunities()
     applications = load_data("applications.json")
 
+    # Attempts to convert user's input into an integer to prevent errors from entering letters
     try:
         choice = int(input("\nEnter opportunity ID to apply for: "))
     except ValueError:
         print("Please enter a number.")
         return
 
+    # Searches for an opportunity with an ID which matches the user's input
     for opportunity in opportunities:
         if opportunity.id == choice:
 
+            # Creates an Application object for the selected opportunity
             new_application = Application(
                 username,
                 opportunity.id,
@@ -129,6 +169,7 @@ def apply_opportunity(username):
                 "Applied"
             )
 
+            # Adds the application information to the application list
             applications.append({
                 "username": new_application.username,
                 "opportunity_id": new_application.opportunity_id,
@@ -136,17 +177,27 @@ def apply_opportunity(username):
                 "status": new_application.status
             })
 
+            # Saves the new application to applications.json
             save_data("applications.json", applications)
 
             print("Application submitted successfully!")
             return
 
+    # Runs if entered ID does not match with any avaliable opportunities
     print("Opportunity not found.")
 
+# Main Program (Main Menu)
+
+# Controls the main flow of the program and determines which menu user should see
+# Menu shown depends on whether there is someone logged in or not
 def main():
+    # Corresponds to no student currently being logged in
     current_user = None
 
+    # Keeps the program running until user chooses to exit
     while True:
+
+        #Displays login menu when no student is currently logged in
         if current_user is None:
             print("\n===== Opportunity Tracker =====")
             print("1. Login")
@@ -155,12 +206,15 @@ def main():
 
             choice = input("Select option: ")
 
+            # Attempts to log student in
             if choice == "1":
                 current_user = login()
 
+            # reates a new account and then logs student in immediately after
             elif choice == "2":
                 current_user = signup()
 
+            # Ends the program
             elif choice == "3":
                 print("Goodbye!")
                 break
@@ -168,6 +222,7 @@ def main():
             else:
                 print("Invalid option.")
 
+        # Displays the student menu/main menu when student is logged in
         else:
             print("\n===== Opportunity Tracker =====")
             print("Welcome,", current_user)
@@ -177,12 +232,15 @@ def main():
 
             choice = input("Select option: ")
 
+            # Option to display all avaliable opportunities currently
             if choice == "1":
                 view_opportunities()
 
+            # Allows student to apply for an opportunity
             elif choice == "2":
                 apply_opportunity(current_user)
 
+            # Logs student out
             elif choice == "3":
                 current_user = None
                 print("Logged out.")
@@ -190,4 +248,5 @@ def main():
             else:
                 print("Invalid option.")
 
+# Starts the program by calling the main function
 main()
